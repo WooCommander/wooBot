3b6a6f7b231e975d11645dadfcc694f64f7d6860
Woocommander <johndoe@example.comocnelavoc@gmail.com>
2025-04-10 07:24:38 +0300

    upd

--- parser-bot.js
+++ parser-bot.js
@@ -2,6 +2,11 @@
 const TelegramBot = require('node-telegram-bot-api');
 const fetch = require('node-fetch'); // ← вот это оставить
 const cheerio = require('cheerio');
+//https://api.telegram.org/bot7752409637:AAGyOahq728RyWlIqYR_8aKZ26IFhFo8asA
+//https://api.telegram.org/bot7752409637:AAGyOahq728RyWlIqYR_8aKZ26IFhFo8asA/getMe
+// ЗАМЕНИ своими данными
+const token = '7752409637:AAGyOahq728RyWlIqYR_8aKZ26IFhFo8as1A';
+const adminId = '5029697145'; // можно узнать через @userinfobot
 
 
 const bot = new TelegramBot(token, { polling: false });
@@ -16,19 +21,21 @@ async function checkMakler() {
     const html = await res.text();
     const $ = cheerio.load(html);
 
-    $('.items-box .item-box').each(async (i, elem) => {
-      const title = $(elem).find('.item-title').text().trim();
-      const price = $(elem).find('.item-price').text().trim();
-      const link = 'https://makler.md' + $(elem).find('a.item-title').attr('href');
+    // $('.items-box .item-box').each(async (i, elem) => {
+    //   const title = $(elem).find('.item-title').text().trim();
+    //   const price = $(elem).find('.item-price').text().trim();
+    //   const link = 'https://makler.md' + $(elem).find('a.item-title').attr('href');
 
-      if (!knownLinks.has(link)) {
-        knownLinks.add(link);
+    //   if (!knownLinks.has(link)) {
+    //     knownLinks.add(link);
 
-        const message = `🚗 *Новое объявление:*\n*${title}*\n💰 ${price}\n🔗 [Открыть объявление](${link})`;
-        await bot.sendMessage(adminId, message, { parse_mode: 'Markdown' });
-      }
-    });
-
+    //     const message = `🚗 *Новое объявление:*\n*${title}*\n💰 ${price}\n🔗 [Открыть объявление](${link})`;
+    //     await bot.sendMessage(adminId, message, { parse_mode: 'Markdown' });
+    //     console.log('message',message);
+    //   }
+    // });
+    console.log('message',"test");
+    await bot.sendMessage(adminId, "test");
     console.log('Проверка завершена');
   } catch (err) {
     console.error('Ошибка при парсинге:', err.message);
@@ -36,4 +43,4 @@ async function checkMakler() {
 }
 
 // Проверка каждые 60 секунд
-setInterval(checkMakler, 60000);
+setInterval(checkMakler, 6000);
